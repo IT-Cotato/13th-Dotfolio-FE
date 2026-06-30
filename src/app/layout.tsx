af40d8f256775336d0e8e67d556df0560a4b7512
idid10 <ejwuud2@naver.com>
2026-06-30 20:40:36 +0900

    fix: 레이아웃 고정 크기를 화면에 맞게 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import Home from './home'
 
 export default function Layout() {
   return (
-    <div className="w-[1440px] h-[1024px]">
+    <div className="w-full h-screen">
       <header className="w-full h-[80px] border-b border-[var(--color-grey-100)]" />
-      <div className="flex h-[calc(1024px-80px)]">
+      <div className="flex h-[calc(100vh-80px)]">
         <nav className="w-[240px] shrink-0 border-r border-[var(--color-grey-100)]" />
         <main className="flex-1 px-6 py-6">
           <Home />

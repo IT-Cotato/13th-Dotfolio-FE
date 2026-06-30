3d2e7f8ca0057cf6b7d23f0daff209a26fccc3a4
idid10 <ejwuud2@naver.com>
2026-06-30 20:44:29 +0900

    feat: React Router v6 설치 및 라우팅 세팅

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router-dom'
 import Home from './home'
 
 export default function Layout() {
@@ -7,7 +8,9 @@ export default function Layout() {
       <div className="flex h-[calc(100vh-80px)]">
         <nav className="w-[240px] shrink-0 border-r border-[var(--color-grey-100)]" />
         <main className="flex-1 px-6 py-6">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
         </main>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './styles/globals.css'
 import Layout from './app/layout'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Layout />
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
   </StrictMode>,
 )

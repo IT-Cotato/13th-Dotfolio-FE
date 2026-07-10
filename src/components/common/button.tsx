3b78d6845a5991445b148e4446b4f80da4067129
idid10 <ejwuud2@naver.com>
2026-07-10 21:12:12 +0900

    refactor: outline variant에서 disabled 상태 무시 이슈 해결

--- src/components/common/button.tsx
+++ src/components/common/button.tsx
@@ -11,10 +11,12 @@ export const Button = ({ label, onClick, disabled = false, variant = 'primary' }
     onClick={onClick}
     disabled={disabled}
     className={`w-full h-13 flex px-5 py-2.5 items-center justify-center rounded-[14px] text-sub2-sb transition-all ${
-      variant === 'outline'
+      disabled
+        ? variant === 'outline'
+          ? 'border border-grey-300 bg-white text-grey-300 cursor-not-allowed'
+          : 'bg-grey-300 cursor-not-allowed text-grey-0'
+        : variant === 'outline'
         ? 'border border-primary-500 bg-white text-primary-500 cursor-pointer'
-        : disabled
-        ? 'bg-grey-300 cursor-not-allowed text-grey-0'
         : 'bg-primary-gradient text-grey-0 cursor-pointer'
     }`}
   >

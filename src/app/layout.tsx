d4a648e348b58ffd71c964dad88cf6ca2aa22c18
idid10 <ejwuud2@naver.com>
2026-07-22 23:52:00 +0900

    fix: 템플릿 배열 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './home'
+import Login from './login'
 import Record from './record'
 import RecordAll from './record-all'
 import TemplateAll from './template-all'

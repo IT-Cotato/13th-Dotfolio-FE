fc12ace1151ae30bab6e0be447f48a97637c0bd2
seo1v <minseoeum14@gmail.com>
2026-07-24 15:11:53 +0900

    fix: Escape 콜백 갱신 시점 수정

--- src/components/memo/hooks/useEscapeKey.ts
+++ src/components/memo/hooks/useEscapeKey.ts
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useLayoutEffect, useRef } from 'react';
 
 export const useEscapeKey = (onEscape: () => void) => {
   const onEscapeRef = useRef(onEscape);
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     onEscapeRef.current = onEscape;
   }, [onEscape]);
 

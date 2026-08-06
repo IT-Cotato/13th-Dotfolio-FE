7e54ea3ce404ffc2af3a8e60c100a184d74135fd
YoungI422 <dlsrudtj@gmail.com>
2026-08-06 23:23:23 +0900

    fix: 체크박스 컴포넌트 키보드 포커스 표시 추가

--- src/components/common/CheckBox.tsx
+++ src/components/common/CheckBox.tsx
@@ -11,14 +11,14 @@ export function Checkbox({ checked, id, onChange }: CheckboxProps) {
     <>
       <input
         checked={checked}
-        className="sr-only"
+        className="peer sr-only"
         id={id}
         onChange={(event) => onChange(event.target.checked)}
         type="checkbox"
       />
       <span
         aria-hidden="true"
-        className={`w-5 h-5 py-[5px] px-[3px] rounded-[4px] border flex items-center justify-center transition-colors ${
+        className={`w-5 h-5 py-[5px] px-[3px] rounded-[4px] border flex items-center justify-center transition-colors peer-focus-visible:outline-2 peer-focus-visible:outline-offset-2 peer-focus-visible:outline-primary-500 ${
           checked
             ? "bg-primary-500 border-primary-500"
             : "bg-white border-grey-200"

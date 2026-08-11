84d0b1577dc8f1ee892eafce4448a16467ae4910
YoungI422 <dlsrudtj@gmail.com>
2026-08-12 04:21:15 +0900

    feat: 인증 화면 우측 브랜딩 로고 추가

---
+++ src/components/login/AuthBrandLogo.tsx
@@ -0,0 +1,17 @@
+import LogoEllipse from "@/assets/logo_Ellipse.svg";
+import LogoRectangle from "@/assets/logo_Rectangle 33479.svg";
+
+export function AuthBrandLogo() {
+  return (
+    <div className="flex size-28 items-center justify-center gap-10 p-9">
+      <div className="flex size-20 shrink-0 items-start justify-between">
+        <LogoEllipse className="size-3 shrink-0" />
+        <span
+          aria-hidden="true"
+          className="h-20 w-2.5 shrink-0 bg-grey-0 shadow-[inset_0_-0.576px_0.576px_0_rgba(255,255,255,0.15)]"
+        />
+        <LogoRectangle className="h-20 w-12 shrink-0" />
+      </div>
+    </div>
+  );
+}

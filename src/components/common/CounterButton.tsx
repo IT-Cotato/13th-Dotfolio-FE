03689d88f0c30c01e3bcbdfbb6e3ea7e770f943d
YoungI422 <dlsrudtj@gmail.com>
2026-08-12 00:49:22 +0900

    fix: 시간 증가 감소 버튼 수정

--- src/components/common/CounterButton.tsx
+++ src/components/common/CounterButton.tsx
@@ -17,7 +17,7 @@ export function CounterButton({
     <button
       type="button"
       aria-label={ariaLabel}
-      className="flex size-8 shrink-0 items-center justify-center gap-2.5 rounded-lg border-[1.5px] border-grey-0 p-[5px] text-grey-0 disabled:cursor-not-allowed disabled:border-grey-500 disabled:text-grey-500"
+      className="flex size-8 shrink-0 items-center justify-center gap-2.5 rounded-lg border-[1.5px] border-[#4E5C7C] p-[5px] text-grey-0 disabled:cursor-not-allowed disabled:border-[#4E5C7C] disabled:text-[#576789]"
       disabled={disabled}
       onClick={onClick}
     >

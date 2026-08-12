d9785757748d945d18f9f169c4df761ea23b5069
YoungI422 <dlsrudtj@gmail.com>
2026-08-13 01:10:55 +0900

    feat: 랜딩 페이지 히어로 섹션 구현

--- src/app/landing.tsx
+++ src/app/landing.tsx
@@ -1,10 +1,11 @@
-import { LandingHeader } from "@/components/landing/LandingHeader";
+import { HeroSection } from "@/components/landing/HeroSection";
 
 export default function Landing() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center bg-[#F6F8FC]">
-      <LandingHeader />
-      <main className="flex w-full flex-col items-center" />
+      <main className="flex w-full flex-col items-center">
+        <HeroSection />
+      </main>
     </div>
   );
 }

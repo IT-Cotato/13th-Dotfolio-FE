de1471a6342d7390b1e4d4d97fe96f2408ce44c8
YoungI422 <dlsrudtj@gmail.com>
2026-08-12 23:25:11 +0900

    feat: 랜딩 페이지 프레임 및 라우팅 설정

---
+++ src/app/landing.tsx
@@ -0,0 +1,9 @@
+export default function Landing() {
+  return (
+    <div className="flex min-h-screen w-full justify-center bg-[#F6F8FC]">
+      <div className="flex w-[1440px] max-w-full flex-col items-center justify-center bg-[#F6F8FC]">
+        <main className="flex w-full flex-col items-center" />
+      </div>
+    </div>
+  );
+}

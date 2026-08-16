4a3bef343be68f54f7a57af46160320bfc9c1b42
YoungI422 <dlsrudtj@gmail.com>
2026-08-17 01:35:48 +0900

    fix: 로그인 및 회원가입 로고 랜딩페이지 연결

--- src/components/login/LoginHeader.tsx
+++ src/components/login/LoginHeader.tsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export function LoginHeader() {
   return (
     <header className="flex h-20 w-full items-center justify-between px-8 py-7">
-      <span className="text-body2-r text-grey-600">Dotfolio</span>
+      <Link to="/" className="text-body2-r text-grey-600">
+        Dotfolio
+      </Link>
     </header>
   );
 }

9836d25d10decedfe4c5242ab09128304bec3aaa
YoungI422 <dlsrudtj@gmail.com>
2026-08-17 01:39:16 +0900

    fix: 로그인 및 회원가입 로고 디자인 수정

--- src/components/login/LoginHeader.tsx
+++ src/components/login/LoginHeader.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function LoginHeader() {
   return (
     <header className="flex h-20 w-full items-center justify-between px-8 py-7">
-      <Link to="/" className="text-body2-r text-grey-600">
+      <Link to="/" className="font-nexon text-logo text-grey-600">
         Dotfolio
       </Link>
     </header>

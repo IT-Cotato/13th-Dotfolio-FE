2b8700f22d617f70fb11f444417bf15395c0d4c4
seo1v <minseoeum14@gmail.com>
2026-08-20 12:17:31 +0900

    style: 메모 생성 버튼 커서 포인터 적용

--- src/components/memo/header/MemoCreateButton.tsx
+++ src/components/memo/header/MemoCreateButton.tsx
@@ -8,7 +8,7 @@ export const MemoCreateButton = ({ onClick }: MemoCreateButtonProps) => (
   <button
     type="button"
     onClick={onClick}
-    className="bg-primary-gradient flex h-11 items-center gap-2 rounded-xl py-2.5 pl-3.5 pr-5 text-sub2-sb text-white"
+    className="bg-primary-gradient flex h-11 cursor-pointer items-center gap-2 rounded-xl py-2.5 pl-3.5 pr-5 text-sub2-sb text-white"
   >
     <AddIcon className="h-5 w-5" />
     <span>메모 생성</span>
